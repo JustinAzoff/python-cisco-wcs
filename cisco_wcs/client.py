5915bda7cd989a27dd6af6b6ed210f27eb709645
Justin Azoff <justin@bouncybouncy.net>
2012-09-06 09:28:07 -0400

    refactor, add search_client

--- cisco_wcs/client.py
+++ cisco_wcs/client.py
@@ -4,6 +4,21 @@ import pyquery
 class LoginFailure(Exception):
     pass
 
+def get_table(html, table_id):
+    p = pyquery.PyQuery(html)
+    table = p(table_id)[0]
+    rows = table.getchildren()
+    header = rows[0]
+    data = rows[1:]
+    col_names = [c.text_content().strip() for c in header.findall("th")]
+
+    ret = []
+    for row in data:
+        cols = [c.text_content().strip() for c in row.findall("td")]
+        ret.append(dict(zip(col_names, cols)))
+        
+    return ret
+
 class Client:
     def __init__(self, host, username, password):
         self.base = "https://%s/webacs/" % host
@@ -23,16 +38,9 @@ class Client:
     def get_client_detail(self, mac):
         params = {'command': 'detail', 'mobileStationMac': mac}
         r = self.s.get(self.base + "monitorClientDetail.do", params=params)
-        p = pyquery.PyQuery(r.text)
-        table = p("#clientAssoListTable")[0]
-        rows = table.getchildren()
-        header = rows[0]
-        data = rows[1:]
-        col_names = [c.text_content().strip() for c in header.findall("th")]
+        return get_table(r.text, "#clientAssoListTable")
 
-        ret = []
-        for row in data:
-            cols = [c.text_content().strip() for c in row.findall("td")]
-            ret.append(dict(zip(col_names, cols)))
-            
-        return ret
+    def search_client(self, mac_or_ip_or_name):
+        params = {'searchType': 'macOrIpOrName', 'searchText': mac_or_ip_or_name}
+        r = self.s.get(self.base + "searchClientAction.do", params=params)
+        return get_table(r.text, "#clientFormListTable")
